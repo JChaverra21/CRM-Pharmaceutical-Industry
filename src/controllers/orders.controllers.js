0c3c6d7a3c3307948d97803817466f378f8a88e4
Mateo Arboleda <teo.arb03@gmail.com>
2022-10-05 12:48:30 -0500

    (feat): implemented the functionality that allows us to register the customer's order.

--- src/controllers/orders.controllers.js
+++ src/controllers/orders.controllers.js
@@ -2,5 +2,26 @@ import {pool} from '../database.js'
 
 export const getOrders = async (req,res) => {
     const [rows] = await pool.query('select *from orders')
+
+    console.log(rows);
     res.json(rows)
+}
+
+export const postOrders = async (req, res) => {
+    const {id_client, name, email, phone, address, products, price} = req.body;
+
+    const [rows] = await pool.query('INSERT INTO orders(id_client, name, email, phone, address, products, price) VALUES (?, ?, ?, ?, ?, ?, ?)',
+    [id_client, name, email, phone, address, products, price])
+
+    console.log(req.body);
+    res.send({
+        id_Order: rows.insertId,
+        id_client, 
+        name, 
+        email, 
+        phone, 
+        address, 
+        products, 
+        price
+    });
 }
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express"
-import { getOrders } from "../controllers/orders.controllers.js"
+import { getOrders, postOrders } from "../controllers/orders.controllers.js"
 
 const router = Router();
 
 router.get('/orders', getOrders);
+
+router.post('/newOrder', postOrders);
 
 export default router
