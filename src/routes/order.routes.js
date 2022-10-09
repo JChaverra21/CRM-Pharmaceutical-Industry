52b5f6f64fcca1452fe66a42d387e06e8d1b9149
Jose <david-restrepo_123@hotmail.es>
2022-10-08 23:46:21 -0500

    (feat): functionality has been implemented that will allow the update of datas of an order

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express"
-import { getOrders, postOrders, deleteOrders } from "../controllers/orders.controllers.js"
+import { getOrders, postOrders, deleteOrders, updateOrders } from "../controllers/orders.controllers.js"
 
 const router = Router();
 
 router.get('/orders', getOrders);
 
 router.post('/newOrder', postOrders);
+
+router.put('/upOrder/:id_Order', updateOrders);
 
 router.delete('/deleteOrder/:id_Order', deleteOrders)
 
